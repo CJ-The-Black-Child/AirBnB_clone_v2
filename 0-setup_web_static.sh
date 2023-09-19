2d087c1aeea2c76b0adf17cc4a21546f64eb893a
CJ-The-Black-Child <congomigue@gmail.com>
2023-09-19 14:49:47 +0000

    Updated script to prepare web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,39 @@
 #!/usr/bin/env bash
 # Sets up the web servers for the deployment of web_Static
 
-echo -e "\e[1;32m START\e[Om"
+echo -e "\e[1;32m START\e[0m"
 
 # Update packages and install Nginx
 sudo apt-get -y update
 sudo apt-get -y install nginx
-echo -e "\e[1;32m Packages updated and Nginx installed\e[Om"
+echo -e "\e[1;32m Packages updated and Nginx installed\e[0m"
 echo
 
 # Configure firewall to allow Nginx HTTP connections
 sudo ufw allow 'Nginx HTTP'
-echo -e "\e[1;32m Allowed incoming Nginx HTTP connectiosn\e[Om"
+echo -e "\e[1;32m Allowed incoming Nginx HTTP connectiosn\e[0m"
 echo
 
 # Create directories
 directories=("/data/web_static/releases/test" "/data/web_static/shared")
 for dir in "${directories[@]}"; do
-	sudo mkdir -p "$dir"
+	if [ ! -d "$dir" ]; then
+		sudo mkdir -p "$dir"
+		sudo chown -R ubuntu:ubuntu "$dir"
+	fi
 done
-echo -e "\e[1;32m Directories created\e[Om"
+echo -e "\e[1;32m Directories created\e[0m"
 echo
 
 # Add a test HTML file
-echo "<h1> welcome to www.congojunior.tech</h1>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-echo -e "\e[1;32m Test HTML file added\e[Om"
+echo "<html>
+      	<head>
+      	</head>
+	<body>
+	    Holberton School
+	</body>
+      </html>" >  /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Test HTML file added\e[0m"
 echo
 
 # Prevent overwriting the current symlink
@@ -32,24 +41,26 @@ if [ -d "/data/web_static/current" ]; then
 	echo "Path /data/web_static/current exists, removing..."
 	sudo rm -rf /data/web_static/current
 fi
-echo -e "\e[1;32m Prevented symlink overwrite\e[Om"
+echo -e "\e[1;32m Prevented symlink overwrite\e[0m"
 echo
 
 # Create a symbolic link
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -hR  ubuntu:ubuntu /data
-echo -e "\e[1;32m Symbolic link created and ownership set \e[Om"
+echo -e "\e[1;32m Symbolic link created and ownership set \e[0m"
 echo
 
 # Update Nginx configuration
 nginx_config="/etc/nginx/sites-available/default"
-nginx_config_line="location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "381$nginx_config_line" "$nginx_config"
-echo -e "\e[1;32m Nginx configuration updated\e[Om"
+nginx_config_line="location /hbnb_static/ {
+	alias /data/web_static/current/;
+}"
+sudo sed -i "/location \/ {/a $nginx_config_line" "$nginx_config"
+echo -e "\e[1;32m Nginx configuration updated\e[0m"
 echo
 
 # Restart now
 sudo service nginx restart
-echo -e "\e[1;32m Nginx restarted\e[Om"
+echo -e "\e[1;32m Nginx restarted\e[0m"
 
-echo -e "\e[1;32m Setup completed successfully\e[Om"
+echo -e "\e[1;32m Setup completed successfully\e[0m"
