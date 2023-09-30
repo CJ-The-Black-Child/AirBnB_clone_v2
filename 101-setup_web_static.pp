fbbfc70c889ae84c2f85aa95249a27c4144fca31
CJ-The-Black-Child <congomigue@gmail.com>
2023-09-30 13:12:02 +0000

    Updated: Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -11,47 +11,47 @@ logoutput   => true,
 
 # Configure firewall to allow Nginx HTTP connections
 exec { 'allow_nginx_http':
-command     => 'ufw allow "Nginx HTTP"',
-path        => '/usr/bin',
-require     => Exec['update_and_install_nginx'],
+command => 'ufw allow "Nginx HTTP"',
+path    => '/usr/bin',
+require => Exec['update_and_install_nginx'],
 }
 
 # Creeate directories
 
-$directories = ["/data/web_static/release/test", "/data/web_static/shared"]
+$directories = ['/data/web_static/release/test', '/data/web_static/shared']
 file { $directories:
-ensure => 'directory'.
+ensure  => 'directory',
 }
 
 # Add a test HTML file
 file { '/data/web_static/releases/test/index.html':
-ensure      => 'file',
-content     => "<h1> welcome to www.congojunior.tech</h1>\n",
-owner       => 'ubuntu',
-group       => 'ubuntu',
-mode        => '0644',
-require     => File['/data/web_static/releases/test'],
+ensure  => 'file',
+content => '<h1> welcome to www.congojunior.tech</h1>\n',
+owner   => 'ubuntu',
+group   => 'ubuntu',
+mode    => '0644',
+require => File['/data/web_static/releases/test'],
 }
 
 # Prevent overwriting the current symlink
 file { '/data/web_static/current':
 ensure => 'link',
 target =>'/data/web_static/releases/test',
-owner => 'ubuntu',
-group => 'ubunt',
-force => true,
+owner  => 'ubuntu',
+group  => 'ubuntu',
+force  => true,
 before => File['/etc/nginx/sites-available/default'],
 }
 
 # Update Nginx configuration
 file { '/etc/nginx/sites-available/default':
-ensure => 'file',
+ensure  => 'file',
 require => Package['nginx'],
 }
 
 # Restart Nginx
 service {'nginx':
-	ensure    => 'running',
-	enable    => true,
-	subscribe => File[ '/etc/nginx/sites-available/default' ],
+ensure    => 'running',
+enable    => true,
+subscribe => File[ '/etc/nginx/sites-available/default' ],
 }
